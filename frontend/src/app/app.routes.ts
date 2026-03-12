cff214ce589bb8b3d04c189c2f5fe336e6d85937
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2026-03-12 13:43:59 +0530

    frontend routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { EmployeeFormComponent } from './components/employees/employee-form/employee-form.component';
 import { EmployeeListComponent } from './components/employees/employee-list/employee-list.component';
+import { AttendanceListComponent } from './components/attendance/attendance-list/attendance-list.component';
 
 export const routes: Routes = [
      {
@@ -16,6 +17,10 @@ export const routes: Routes = [
 {
   path: 'add-employee',
   component: EmployeeFormComponent
+},
+{
+  path: 'attendance',
+  component: AttendanceListComponent
 }
 
 ];
