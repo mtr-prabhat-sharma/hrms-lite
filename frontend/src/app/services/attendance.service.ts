f8a77f663440cb2815eefbf80431900a24cecbd7
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2026-03-13 09:04:46 +0530

    updated imports

--- frontend/src/app/services/attendance.service.ts
+++ frontend/src/app/services/attendance.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Attendance } from '../models/attendance.model';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
--- frontend/src/app/services/employee.service.ts
+++ frontend/src/app/services/employee.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Employee } from '../models/employee.model';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 
 
 @Injectable({
